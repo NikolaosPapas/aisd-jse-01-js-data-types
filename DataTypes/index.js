1e23609ed8b3871ffeca9bfaab024a44c87fa47f
NikolaosPapas <nico.mendivil1@gmail.com>
2024-10-28 19:33:03 -0700

    added JS

---
+++ DataTypes/index.js
@@ -0,0 +1,36 @@
+// String
+const greeting = "Hello, World!";
+console.log(greeting); // Expected Expected Output: Hello, World!
+
+// Example of a number in a string
+const myAgeAsAString = "24"; 
+console.log(myAgeAsAString); // Expected Output: "24"
+console.log(typeof myAgeAsAString); // Expected Output: "string"
+
+
+// Boolean
+const isStudent = true;
+const hasGraduated = false;
+console.log(isStudent); // Expected Output: true
+console.log(hasGraduated); // Expected Output: false
+
+
+// Undefined
+let user; // Declared but not assigned
+console.log(user); // Expected Output: undefined
+
+// Null
+const nullUser = null;
+console.log(nullUser) // Expected Output: "null"
+
+// Array
+const people = ["Alice", "Bob", "Charlie"];
+console.log(people); // Expected Output: ["Alice", "Bob", "Charlie"]
+
+// Object
+const person = {
+    name: "Alice",
+    age: 22,
+    isStudent: false
+  };
+  console.log(person); // Expected Output: { name: 'Alice', age: 22, isStudent: false }
